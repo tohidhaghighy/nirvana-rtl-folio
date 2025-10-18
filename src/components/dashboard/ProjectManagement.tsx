8947bd88bd9cd28fb5e1323c804822c8d5125c5d
shakilakp <kazempour.shakila@gmail.com>
2025-10-18 16:25:22 +0330

    routing edit

--- src/components/dashboard/ProjectManagement.tsx
+++ src/components/dashboard/ProjectManagement.tsx
@@ -91,7 +91,7 @@ const ProjectManagement = () => {
 
     const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-    const response = await fetch(`${API_BASE_URL}/uploads/project-image`, {
+    const response = await fetch(`${API_BASE_URL}/project-image`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
